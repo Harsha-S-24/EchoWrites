45e391f7eef591d5e2e54462ab5674a34c94bc34
Harsha <HarshaSridhar888@gmail.com>
2024-07-19 23:32:56 +0530

    Changed Quote

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -4,12 +4,11 @@ export const Quote = () => {
       <div className="flex justify-center">
         <div className="max-w-lg  ">
           <div className="text-3xl font-bold">
-            "The Customer Support I Recieved was Exceptional. The support team
-            went above and beyond to express my concerns"
+            "Your story matters. Join our community of storytellers and let your voice be echoed around the world."
           </div>
           <div className="max-w-md  text-xl font-semibold mt-4">Harsha</div>
           <div className="max-w-md text-md font-light text-slate-400">
-            CEO | World
+            CEO | EchoWrites
           </div>
         </div>
       </div>
